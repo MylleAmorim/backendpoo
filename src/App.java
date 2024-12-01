93b2364e6cfb4f0cf7da4027daaf41d730984641
MylleAmorim <111822365+MylleAmorim@users.noreply.github.com>
2024-12-01 09:31:04 -0300

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,35 @@
+package src;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Criando nosso Banco Digital");
+        System.out.println();
+
+
+
+        ContaBancaria conta = new ContaBancaria (0001, 1904 , 5 , 100 );
+        System.out.println("Saldo atual de R$" + conta.getSaldo());
+        System.out.println();
+
+        conta.depositar(250.0);
+        System.out.println("Saldo atual de R$" + conta.getSaldo());
+        System.out.println();
+
+        var saque = conta.sacar(150.0);
+        System.out.println("Saldo atual de R$" + conta.getSaldo());
+        System.out.println();
+
+
+
+
+
+
+    
+
+
+    
+        
+    }
+    
+}
