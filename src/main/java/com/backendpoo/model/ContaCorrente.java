fcc7fa6c4ce70484b35cf7e89615421a0578a560
Jamylle <emaildajamylle123@gmail.com>
2024-12-02 16:00:27 -0300

    update

---
+++ src/main/java/com/backendpoo/model/ContaCorrente.java
@@ -0,0 +1,13 @@
+package com.backendpoo.model;
+
+public class ContaCorrente extends ContaBancaria {
+
+    public ContaCorrente ( String agencia, String conta , Integer digito,Double saldoInicial) {
+
+        super(agencia,conta,digito,saldoInicial);
+
+
+
+
+    }
+}
